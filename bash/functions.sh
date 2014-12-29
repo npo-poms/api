a8a90df0328fea6892637eabb4d80646c1eba239
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2014-12-29 14:31:57 +0100

    search in bash

---
+++ bash/functions.sh
@@ -0,0 +1,29 @@
+rawurlencode() {
+  local string="${1}"
+  local strlen=${#string}
+  local encoded=""
+
+  for (( pos=0 ; pos<strlen ; pos++ )); do
+     c=${string:$pos:1}
+     case "$c" in
+        [-_.~a-zA-Z0-9] ) o="${c}" ;;
+        * )               printf -v o '%%%02x' "'$c"
+     esac
+     encoded+="${o}"
+  done
+  echo "${encoded}"    # You can either set a return variable (FASTER)
+  REPLY="${encoded}"   #+or echo the result (EASIER)... or both... :p
+}
+
+
+date() {
+    gdate --rfc-822
+}
+
+authenticateHeader() {
+    #$1: date $2: uri
+    message="origin:$origin,x-npo-date:$1,uri:$2"
+    #echo "$message\n"
+    base64=`python -c "import hmac, hashlib,base64,sys; print base64.b64encode(hmac.new(b\"$secret\", msg=\"$message\", digestmod=hashlib.sha256).digest())"`
+    echo "NPO $apiKey:$base64"
+}
---
+++ bash/media/search.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#set -x
+
+source ../creds.sh
+source ../functions.sh
+
+
+call="api/media/"
+uri="/v1/$call"
+npodate=$(date)
+header=$(authenticateHeader "$npodate" "$uri")
+echo $header
+curl -H "Authorization: $header"  -H "X-NPO-Date: $npodate" -H "Origin: $origin"  "$baseUrl$call" -X POST
+echo $!
