8b3d74e30d4d4769b8ab71a5a836db901535b12f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2014-12-29 16:43:33 +0100

    test also mediaForm inside page form

--- bash/pages/test-all.sh
+++ bash/pages/test-all.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
 mkdir -p testresults
-for i in `ls ../../examples/pages` ; do
-    target=testresults/$i
-    echo "$i -> $target"
-    ./search.sh ../../examples/pages/$i | jsonformat > $target
+for form in `ls ../../examples/pages` ; do
+    target=testresults/$form
+    echo "$form -> $target"
+    ./search.sh ../../examples/pages/$form | jsonformat > $target
+done
+
+
+#also embed entire media forms
+mkdir -p testresults/media
+mkdir -p testresults/forms
+for mediaForm in `ls ../../examples/media` ; do
+    target=testresults/media/$mediaForm
+    echo "$mediaForm -> $target"
+    pageForm=testresults/forms/$mediaForm
+
+    echo " {    \"mediaForm\" :" >  $pageForm
+    cat ../../examples/media/$mediaForm >> $pageForm
+    echo "}" >> $pageForm
+    echo "Using $pageForm";
+    ./search.sh $pageForm | jsonformat > $target
 done
