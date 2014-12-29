9f89203c1975a27c0a9cccb3e271406680ed7a13
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2014-12-29 15:15:45 +0100

    comments

--- bash/functions.sh
+++ bash/functions.sh
@@ -26,6 +26,7 @@ authenticateHeader() {
     for param in $3 ; do
         message="$message,${param//=/:}"
     done
+    #note: $secret and $apiKey variables are coming from creds.sh, which should have been included
     # should be possible with openssl, but I can't get it working. This is in python
     base64=`python -c "import hmac, hashlib,base64,sys; print base64.b64encode(hmac.new(b\"$secret\", msg=\"$message\", digestmod=hashlib.sha256).digest())"`
     echo "NPO $apiKey:$base64"
