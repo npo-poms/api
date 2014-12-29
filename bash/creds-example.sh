b6cbe18b20388e25bdda99699d81e0cd3fe923ba
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2014-12-29 14:36:29 +0100

    example on how to make an creds.sh

---
+++ bash/creds-example.sh
@@ -0,0 +1,11 @@
+#publisher
+rs=http://publish-dev.pages.omroep.nl/api
+user=vpro-cms:<password>
+
+
+# npo api
+baseUrl=http://api-dev.poms.omroep.nl/v1/
+
+apiKey=<your key>
+secret=<your secret>
+origin=http://www.vpro.nl
