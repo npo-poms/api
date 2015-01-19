5103adf92f41052450c48e61767377b52125efac
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-01-19 15:02:14 +0100

    simplified

--- bash/creds-example.sh
+++ bash/creds-example.sh
@@ -1,11 +1,7 @@
 #publisher
-rs=http://publish-dev.pages.omroep.nl/api
 user=vpro-cms:<password>
 
-
 # npo api
-baseUrl=http://api.poms.omroep.nl/v1/
-
 apiKey=<your key>
 secret=<your secret>
 origin=http://www.vpro.nl
