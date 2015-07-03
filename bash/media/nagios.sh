105adb792e4e4e58094ca74e2435456c9b763490
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-07-03 09:47:21 +0200

    nagios script

---
+++ bash/media/nagios.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#set -x
+
+start=$(date '+%s')
+
+SOURCE=$(readlink  $BASH_SOURCE)
+if [[ -z "$SOURCE" ]] ; then
+    SOURCE=$BASH_SOURCE
+fi
+source $(dirname ${SOURCE[0]})/../api-functions.sh
+
+if [[ "$1" == ""  ]] ; then
+    echo Usage:
+    echo "[ENV=<prod|test|dev>] $0 <MID>"
+    exit
+fi
+
+
+temp=$(mktemp -t "$0")
+# find the implementation of the post function in ../api-functions.sh
+get "api/media/$1" > $temp
+bytes=$(cat $temp | wc -c)
+rm $temp
+if [ "$status" == "200" ] ; then
+    end=$(date '+%s')
+    diff=$(echo "$end - $start" | bc)
+    echo "OK: HTTP/1.1 $status OK -  $bytes bytes in $diff seconds response time"
+    exit 0;
+else
+    echo "NOT OK: HTTP/1.1 $status -  $bytes bytes in $diff seconds response time"
+    exit 2;
+fi
