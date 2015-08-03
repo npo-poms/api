50539c2d683aabc4990acc49ebb19655cc0930fe
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-08-03 17:07:35 +0200

    better php example

---
+++ php/creds-example.php
@@ -0,0 +1,6 @@
+edit creds-example.php and move to ../../creds.php
+<?php
+$apiKey="myapikey";
+$secret="myapisecret";
+$origin="http://kassa.vara.nl";
+?>
--- php/get.php
+++ php/get.php
@@ -9,7 +9,6 @@ $request = new Ntr\PomsBundle\Poms\Request(array(
                                                  'npo_api_secret' => $secret,
                                                  'npo_api_origin' => $origin));
 
-echo "media/" . $argv[1];
 $result = $request->setOperation('media/' . $argv[1])->get();
-echo var_dump($result);
+echo json_encode($result);
 ?>
