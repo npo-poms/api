a15b4d9c5b00c5ea3bf6ddaee1d8c45668166323
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-08-17 15:45:09 +0200

    add possibillity to specifiy profile

--- bash/media/search.sh
+++ bash/media/search.sh
@@ -16,15 +16,19 @@ fi
 
 
 parameters="max=$MAX" # make sure they are ordered!
-
+if [[ "$2" != "" ]] ; then
+    parameters="max=$MAX&profile=$2"
+fi
 
 if [[ "$1" == "" ||  ! -e $1 ]] ; then
     echo Usage:
-    echo "[ENV=<prod|test|dev>] $0 [<json file with search form>]"
+    echo "[ENV=<prod|test|dev>] $0 <json file with search form> [<profile>]"
     echo "e.g.: "
     echo " $0 $(dirname ${SOURCE[0]})/../../examples/media/example1.json | jsonformat"
     exit
 fi
+
+
 
 # find the implementation of the post function in ../api-functions.sh
 post "api/media" $parameters $1
