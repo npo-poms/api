023c93013f283eab2fa7623d97880221f3d698ee
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-08-19 18:18:42 +0200

    indentation only

--- bash/media/show-related.sh
+++ bash/media/show-related.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 #set -x
-
-SOURCE=$(readlink  $BASH_SOURCE)
-if [[ -z "$SOURCE" ]] ; then
-    SOURCE=$BASH_SOURCE
-fi
-DIR=$(dirname ${SOURCE[0]});
-
+DIR="${BASH_SOURCE%/*}"
 
 $DIR/get.sh $1 | jsongrep -recordsep '  ' -output VALUE "mid,titles[0].value,titles[1].value"
+
 echo -e "\nRelated:"
-MAX=10 $DIR/related.sh $1 | jsongrep -output VALUE -sep ' ' -recordsep $'\n' -record "items.*" "items.*.mid,items.*.objectType,items.*.titles[0].value,items.*.titles[1].value" | sed -e 's/^/  /'
+
+MAX=10 $DIR/related.sh $1 | jsongrep -output VALUE \
+                                     -sep ' ' -recordsep $'\n'\
+                                     -record "items.*" \
+                                     "items.*.mid,items.*.objectType,items.*.titles[0].value,items.*.titles[1].value" \
+    | sed -e 's/^/  /'
