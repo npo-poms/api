e2f50e2823d450f3ec0f6c39559ab1f6bba3b58f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-10-30 17:40:11 +0100

    more work on python client

--- python/npoapi/npoapi.py
+++ python/npoapi/npoapi.py
@@ -3,6 +3,9 @@ __author__ = 'michiel'
 import hmac, hashlib, base64
 from email import utils
 import urllib.request
+import logging
+import json
+import xml.etree.ElementTree
 
 
 class NpoApi:
@@ -13,66 +16,170 @@ class NpoApi:
 
     def login(self, key, secret):
         self.key = key
-        self.secret= secret
+        self.secret = secret
+        return self
+
+    def env(self, e):
+        if e == "prod":
+            self.url = "https://rs.poms.omroep.nl/v1"
+        elif e == "test":
+            self.url = "https://rs-test.poms.omroep.nl/v1"
+        elif e == "dev":
+            self.url = "http://rs-dev.poms.omroep.nl/v1"
+        elif e == "localhost":
+            self.url = "http://localhost:8070/v1"
+        else:
+            self.url = e
         return self
 
     def configured_login(self):
-        import configparser
+        """
+        Logs in using configuration file. Considered using json (no comments-> unusable) or configparser (nearly properties, but heading are required..)
+        So, now it simply parses the file itself.
+        """
         import os
 
-        config_file = os.path.join(os.path.dirname(__file__), "..", "..", "..", "creds.json")
-        if not os.path.isfile(config_file):
-            config_file = os.path.join(os.path.dirname(__file__),  "creds.json")
+        config_files = [
+            os.path.join(os.path.expanduser("~"), "conf", "creds.properties"),
+            os.path.join(os.path.dirname(__file__), "..", "..", "..", "creds.properties"),
+            os.path.join(os.path.dirname(__file__), "..", "..", "..", "creds.sh"),
+            os.path.join(os.path.dirname(__file__), "creds.properties")]
+        i = 0
+        while not os.path.isfile(config_files[i]):
+            i += 1
 
-        with open(config_file) as data:
-            json_data = json.load(data, strict = False)
-            self.login(json_data["apikey"], json_data["secret"])
-            if "origin" in json_data:
-                self.origin = json_data["origin"]
+        config_file = os.path.normpath(config_files[i])
+        logging.debug("Reading " + config_file)
+
+        settings = {}
+        with open(config_file, "rt") as f:
+            for line in f:
+                l = line.strip()
+                if l and not l.startswith("#"):
+                    key_value = l.split("=", 2)
+                    settings[key_value[0].strip().lower()] = key_value[1].strip('" \t')
+        logging.debug(str(settings))
+        self.login(settings["apikey"], settings["secret"])
+        if "origin" in settings:
+            self.origin = settings["origin"]
         return self
 
+    def authenticate(self, uri=None, now=utils.formatdate()):
+        message = "origin:" + self.origin + ",x-npo-date:" + now + ",uri:/v1" + uri
+        logging.debug("message:" + message)
+        encoded = base64.b64encode(
+            hmac.new(self.secret.encode('utf-8'), msg=message.encode('utf-8'), digestmod=hashlib.sha256).digest())
+        return "NPO " + self.key + ":" + encoded.decode('utf-8'), now
 
+    def _get_url(self, path, params=None):
+        if not params:
+            params = {}
 
+        path_for_authentication = path
+        if params.items():
+            sep = "?"
+            for k, v in sorted(params.items()):
+                path += sep + k + "=" + urllib.request.quote(str(v))
+                path_for_authentication += "," + k + ":" + str(v)
+                sep = "&"
 
-    def authenticate(self, uri=None):
-        now = utils.formatdate()
-        message = "origin:" + self.origin + ",x-npo-date:" + now + ",uri:/v1/" + uri
-        encoded = base64.b64encode(hmac.new(self.secret.encode('ascii'), msg=message.encode('ascii'), digestmod=hashlib.sha256).digest())
-        return "NPO " + self.key + ":" + encoded.decode('ascii'), now
+        url = self.url + path
+        return url, path_for_authentication
 
-    def http_get(self, path, params=None):
-        req = urllib.request.Request(self.url)
-        authorization, date = self.authenticate(path)
+    def _authentication_headers(self, req, path_for_authentication):
+        authorization, date = self.authenticate(path_for_authentication)
         req.add_header("Authorization", authorization)
         req.add_header("X-NPO-Date", date)
         req.add_header("Origin", self.origin)
-        req.add_header("Accept", "application/json")
-        return urllib.request.urlopen(req).read()
 
+        logging.debug(str(req.get_full_url()))
+
+    @staticmethod
+    def _get_data(data=None):
+        if type(data) == str:
+            try:
+                json_object = json.JSONDecoder().decode(data)
+                return json.JSONEncoder().encode(json_object).encode("UTF-8"), "application/json"
+            except json.JSONDecodeError:
+                return data.encode("UTF-8"), "application/xml"
+
+
+
+        return None,None
+
+    def http_get(self, path, params=None, accept="application/json", data=None):
+        url, path_for_authentication = self._get_url(path, params)
+        d, ct = self._get_data(data)
+        req = urllib.request.Request(url, data=d)
+        if ct:
+            req.add_header("Content-Type", ct)
+
+        self._authentication_headers(req, path_for_authentication)
+        req.add_header("Accept", accept)
+        return urllib.request.urlopen(req).read().decode('utf-8')
+
+
+class Media(NpoApi):
     def get(self, mid):
-        return self.http_get("api/media/" + mid)
+        return self.http_get("/api/media/" + mid)
+
+    def list(self):
+        return self.http_get("/api/media")
+
+    def search(self, form="{}", sort="asc", offset=0, max_=240):
+        return self.http_get("/api/media", data=form, params={"sort": sort, "offset": offset, "max": max_})
+
+
+class Pages(NpoApi):
+    def get(self, url):
+        return self.http_get("/api/page/" + url)
+
+
+class Screens(NpoApi):
+    def list(self, sort="asc", offset=0, max_=240):
+        return self.http_get("/api/screens", params={"sort": sort, "offset": offset, "max": max_})
 
 
 import unittest
 
-class Tests(unittest.TestCase):
-    def test_authenication(self):
-        npoapi = NpoApi(origin ="http://www.vpro.nl") \
-            .login(key="a", secret="b")
-        npoapi.email = 'michiel.meeuwissen@gmail.com'
+ENV = "dev"
 
-        self.assertEqual("NPO ", npoapi.authenticate(uri = "/media/"))
+
+class Tests(unittest.TestCase):
+
+    def test_authentication(self):
+        client = NpoApi(origin="http://www.vpro.nl") \
+            .login(key="a", secret="b")
+        self.assertEqual("NPO a:CtHYR9a+nr17OIn5rYml6a+A9ujqe0IywWqr93/DAOk=",
+                         client.authenticate(uri="/media", now="Fri, 30 Oct 2015 08:43:31 -0000")[0])
+
+
+class MediaTests(unittest.TestCase):
 
     def test_get(self):
-        npoapi = NpoApi().configured_login()
+        client = Media().configured_login().env(ENV)
+        result = json.JSONDecoder().decode(client.get("AVRO_1656037"))
+        self.assertEqual(result["mid"], "AVRO_1656037")
 
-        npoapi.email, npoapi.origin = "michiel.meeuwissen@gmail.com", "http://www.vpro.nl"
-        print(npoapi.get("AVRO_1656037"))
+    def test_list(self):
+        client = Media().configured_login().env(ENV)
+        result = json.JSONDecoder().decode(client.list())
+        print(result)
 
-    def test_configured_login(self):
-        npoapi = NpoApi().configured_login()
-        npoapi.email, npoapi.origin = "michiel.meeuwissen@gmail.com", "http://www.vpro.nl"
-        print(npoapi.authenticate(uri = "/media"))
+    def test_search(self):
+        client = Media().configured_login().env(ENV)
+        result = json.JSONDecoder().decode(client.search("{}"))
+        print(result)
+
+
+class ScreenTests(unittest.TestCase):
+    def test_screens(self):
+        client = Screens().configured_login().env(ENV)
+        result = json.JSONDecoder().decode(client.list(offset=3))
+        self.assertEqual(result["offset"], 3)
+
+
+
 
 
 
---
+++ python/npoapi/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='NPO API',
+    version='0.2dev',
+    packages=['.', ],
+    long_description=open('README.txt').read(),
+)
