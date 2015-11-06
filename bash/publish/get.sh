b8659a3923466dbaaeb599c259f5c16ec8102d8c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-11-06 11:11:28 +0100

    a get

---
+++ bash/publish/get.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ -z "$1" ] ; then
+    echo use "$0 <update.xml>"
+    exit
+fi
+
+
+source $(dirname ${BASH_SOURCE[0]})/functions.sh
+
+target="$(getUrl)/pages/updates"
+
+echo $target >&2
+
+
+curl -i -s --insecure --user $user --header "Content-Type: application/xml" --header "Accept: application/xml" -X POST --data @$1 \
+    ${target}
