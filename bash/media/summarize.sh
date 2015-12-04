480d883b457bc0ac6b73cdc7bc4940a8017265f9
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-12-04 17:07:10 +0100

    using date formatting summarize

--- bash/media/summarize.sh
+++ bash/media/summarize.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 jsongrep -output VALUE \
-         -sep ' ' \
+         -sep $'\t' \
          -recordsep $'\n'\
          -record 'items.*' \
-         ".result.mid,.result.objectType,.result.titles[0].value,.result.broadcasters[0].id,.score" \
-    | sed -e 's/^/  /'
+         ".result.mid,.result.objectType,.result.titles[0].value,.result.broadcasters[0].id,.result.scheduleEvents[0].channel,.result.scheduleEvents[0].start,.score" \
+    | perl -lan -F'\t' -e 'use DateTime; print "$F[0]\t$F[1]\t$F[2]\t$F[3]\t($F[4]\t".DateTime->from_epoch(epoch=>$F[5] / 1000).")\t$F[6]"'
