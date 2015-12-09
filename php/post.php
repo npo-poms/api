3c429fbe79da476392e647dedcf72d27d3442242
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2015-12-09 14:19:08 +0100

    created post example

---
+++ php/post.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+include 'pomsbundle/Request.php';
+include '../../creds.php';
+
+$request = new Ntr\PomsBundle\Poms\Request(array(
+                                                 'npo_api_key' => $apiKey,
+                                                 'npo_api_secret' => $secret,
+                                                 'npo_api_origin' => $origin));
+
+$file = $argv[1];
+$body = file_get_contents($file);
+echo $body;
+$result = $request->setOperation('media')->setBody($body, False)->addParameter("offset", 0)->addParameter("max", 10)->post();
+echo json_encode($result);
+?>
