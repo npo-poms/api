ea9da3001997f1cefafe405e26d9354ae9e536b7
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-06-17 12:59:45 +0200

    Silly defaults.

--- java/followchanges/src/main/java/nl/vpro/poms/followchanges/Main.java
+++ java/followchanges/src/main/java/nl/vpro/poms/followchanges/Main.java
@@ -1,14 +1,12 @@
 package nl.vpro.poms.followchanges;
 
-import nl.vpro.api.client.frontend.NpoApiClients;
-import nl.vpro.api.client.utils.NpoApiMediaUtil;
-import nl.vpro.domain.api.Deletes;
-import nl.vpro.domain.api.MediaChange;
-import nl.vpro.domain.api.Order;
-import nl.vpro.util.CountedIterator;
-
 import java.time.Duration;
 import java.time.Instant;
+
+import nl.vpro.api.client.frontend.NpoApiClients;
+import nl.vpro.api.client.utils.NpoApiMediaUtil;
+import nl.vpro.domain.api.*;
+import nl.vpro.util.CountedIterator;
 
 import static nl.vpro.util.Env.PROD;
 
@@ -19,32 +17,32 @@ import static nl.vpro.util.Env.PROD;
 public class Main {
 
 
-	public static void main(String[] argv) throws Exception {
-		NpoApiClients clients = NpoApiClients
+    public static void main(String[] argv) throws Exception {
+        NpoApiClients clients = NpoApiClients
             .configured(PROD)
             .build();
-		NpoApiMediaUtil mediaUtil = NpoApiMediaUtil.builder()
+        NpoApiMediaUtil mediaUtil = NpoApiMediaUtil.builder()
             .clients(clients)
             .build();
-		Instant start = Instant.now().minus(Duration.ofMinutes(10));
+        Instant start = Instant.now().minus(Duration.ofMinutes(10));
         String mid = null;
         while(true) {
 
-			try (CountedIterator<MediaChange> changes = mediaUtil.changes(null, false, start, mid, Order.ASC, null, Deletes.ID_ONLY)) {
-				while (changes.hasNext()) {
-					MediaChange change = changes.next();
-					if (!change.isTail()) {
-						System.out.println(change);
-					} else {
+            try (CountedIterator<MediaChange> changes = mediaUtil.changes(null, false, start, mid, Order.ASC, null, Deletes.ID_ONLY)) {
+                while (changes.hasNext()) {
+                    MediaChange change = changes.next();
+                    if (!change.isTail()) {
+                        System.out.println(change);
+                    } else {
 
-					}
-					start = change.getPublishDate();
-					mid = change.getMid();
-				}
-				//log.info("sleeping");
-				Thread.sleep(1000L);
-			}
-		}
+                    }
+                    start = change.getPublishDate();
+                    mid = change.getMid();
+                }
+                //log.info("sleeping");
+                Thread.sleep(1000L);
+            }
+        }
 
-	}
+    }
 }
