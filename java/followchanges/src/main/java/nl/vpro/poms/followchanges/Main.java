9909b2dcf3e4ba9a58cbc3079194fb757613913b
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-06-17 23:32:28 +0200

    Details.

--- java/followchanges/src/main/java/nl/vpro/poms/followchanges/Main.java
+++ java/followchanges/src/main/java/nl/vpro/poms/followchanges/Main.java
@@ -1,5 +1,6 @@
 package nl.vpro.poms.followchanges;
 
+import lombok.extern.slf4j.Slf4j;
 import nl.vpro.api.client.frontend.NpoApiClients;
 import nl.vpro.api.client.utils.NpoApiMediaUtil;
 import nl.vpro.domain.api.Deletes;
@@ -12,21 +13,23 @@ import nl.vpro.util.Env;
 import java.io.FileDescriptor;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.Instant;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static nl.vpro.util.Env.PROD;
 
 /**
  * @author Michiel Meeuwissen
  * @since ...
  */
+@Slf4j
 public class Main {
 
 
+
     public static void main(String[] argv) throws InterruptedException {
-        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, StandardCharsets.UTF_8));
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, UTF_8));
 
 
         NpoApiClients clients = NpoApiClients
@@ -40,17 +43,17 @@ public class Main {
         int call = 0;
         while(true) {
 
-            try (CountedIterator<MediaChange> changes = mediaUtil.changes(null, false, start, mid, Order.ASC, null, Deletes.ID_ONLY, Tail.ALWAYS)) {
+            try (CountedIterator<MediaChange> changes = mediaUtil.changes(null, false, start, mid, Order.ASC, null, Deletes.ID_ONLY, Tail.ALWAYS))  {
                 while (changes.hasNext()) {
                     MediaChange change = changes.next();
-                    System.out.println(call + ":" + change);
+                    log.info(call + ":" + change);
                     start = change.getPublishDate();
                     mid = change.getMid();
                 }
-                call++;
             } catch (Exception e) {
-                System.err.println(e.getMessage());
+                log.info(e.getMessage());
             }
+            call++;
             Thread.sleep(1000L);
         }
 
