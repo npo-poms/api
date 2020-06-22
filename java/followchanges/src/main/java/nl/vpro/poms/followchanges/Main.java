30d2bd8583c6d7f5bcb899df50b3cd6ab5b8684f
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2020-06-22 09:37:05 +0200

    Cleaned up a bit.

--- java/followchanges/src/main/java/nl/vpro/poms/followchanges/Main.java
+++ java/followchanges/src/main/java/nl/vpro/poms/followchanges/Main.java
@@ -1,5 +1,6 @@
 package nl.vpro.poms.followchanges;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import nl.vpro.api.client.frontend.NpoApiClients;
 import nl.vpro.api.client.utils.NpoApiMediaUtil;
@@ -9,17 +10,15 @@ import nl.vpro.domain.api.Order;
 import nl.vpro.domain.api.Tail;
 import nl.vpro.util.CountedIterator;
 import nl.vpro.util.Env;
-import nl.vpro.util.TimeUtils;
+import nl.vpro.util.picocli.Picocli;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.ITypeConverter;
 import picocli.CommandLine.Option;
 
 import java.io.FileDescriptor;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.time.Instant;
-import java.util.concurrent.Callable;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -29,25 +28,20 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 @Slf4j
 @Command(name = "followchange", mixinStandardHelpOptions = true, version = "1.0",
          description = "Follows the media changes feed of the NPO API")
-public class Main implements Callable<Integer> {
+public class Main implements Runnable {
 
 
-    @Option(names = {"-e", "--env"}, converter = EnvConvert.class)
+    @Option(names = {"-e", "--env"}, converter = Picocli.EnvConverter.class)
     private Env env = Env.PROD;
 
     @Option(names = {"-p", "--profile"})
-    private String profile;
+    private String profile = null;
 
-    @Option(names = {"-s", "--since"}, converter = InstantConvert.class)
+    @Option(names = {"-s", "--since"}, converter = Picocli.InstantConvert.class)
     private Instant since = Instant.now();
 
-    @Override
-    public Integer call() throws Exception {
-         changes();
-         return 0;
-    }
-
-    private void changes() throws InterruptedException {
+    @SneakyThrows
+    public void run() {
         NpoApiClients clients = NpoApiClients
             .configured(env)
             .build();
@@ -75,26 +69,9 @@ public class Main implements Callable<Integer> {
         }
     }
 
-
-
-    public static void main(String[] argv) throws InterruptedException {
+    public static void main(String[] argv) {
         // output utf-8 always, why would you want it differently.
         System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, UTF_8));
         System.exit(new CommandLine(new Main()).execute(argv));
-
-
-    }
-
-    public static class EnvConvert implements ITypeConverter<Env> {
-        @Override
-        public Env convert(String value) {
-            return Env.valueOf(value.toUpperCase());
-        }
-    }
-    public static class InstantConvert implements ITypeConverter<Instant> {
-        @Override
-        public Instant convert(String value) {
-            return TimeUtils.parse(value).orElse(Instant.now());
-        }
     }
 }
