9570d3c45790ab15966f7c6130f0174e471b7ce7
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-08-13 16:38:39 +0200

    Better method name

--- typescript/test.ts
+++ typescript/test.ts
@@ -5,10 +5,12 @@ dotenv.config();
 
 const secret = process.env.NPOAPI_SECRET;
 const key = process.env.NPOAPI_KEY;
+const origin = process.env.NPOAPI_ORIGIN;
 
-const api = new NpoApi(key, secret, "https://npo.nl")
 
-api.get("AVRO_1656037?properties=all").then(response => {
+const api = new NpoApi(key, secret, origin);
+
+api.get("AVRO_1656037?properties=ageRating").then(response => {
     console.log(response.data);
 }).catch(error => {
     console.error(error);
