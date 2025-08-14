480a0c007bce6626d1a059b009034109231824d2
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-08-14 10:30:36 +0200

    Removed max.

--- typescript/test.ts
+++ typescript/test.ts
@@ -9,7 +9,7 @@ const origin: string = process.env.NPOAPI_ORIGIN;
 
 const api = new NpoApi(key, secret, origin, 'https://rs-test.poms.omroep.nl/v1/api/');
 
-api.iterate( {properties: "ageRating,predictions", max: "100"}, {
+api.iterate( {properties: "ageRating,predictions"}, {
     searches: {
         type: 'BROADCAST',
         platform: [ 'INTERNETVOD', 'EXTRA' ]
