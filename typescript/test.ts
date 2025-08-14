2566fe6f4fef66c2b009900bd690607ca7ec4ffc
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-08-14 10:26:04 +0200

    Test agains rs-test

--- typescript/test.ts
+++ typescript/test.ts
@@ -7,7 +7,7 @@ const secret: string = process.env.NPOAPI_SECRET;
 const key: string  = process.env.NPOAPI_KEY;
 const origin: string = process.env.NPOAPI_ORIGIN;
 
-const api = new NpoApi(key, secret, origin);
+const api = new NpoApi(key, secret, origin, 'https://rs-test.poms.omroep.nl/v1/api/');
 
 api.iterate( {properties: "ageRating,predictions", max: "100"}, {
     searches: {
