4938a477c3224dacd2973d2fb952801bcd29bf18
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-08-14 12:50:28 +0200

    made it a bitg nicer.

--- typescript/src/npoapi.ts
+++ typescript/src/npoapi.ts
@@ -11,7 +11,7 @@ import CryptoJS from 'crypto-js';
  */
 class NpoApi {
     constructor(private key: string,
-                private secret:string,
+                private secret: string,
                 private origin: string,
                 public base_url: string = "https://rs.poms.omroep.nl/v1/api/"
                 ) {
@@ -21,41 +21,28 @@ class NpoApi {
      * Get one media object by mid
      */
     public async get(mid : string, parameters:Record<string, string> = {}): Promise<any> {
-        let query = this.joinParameters(parameters);
-        let url = this.base_url + "media/" + mid + query;
-        let headers =  this.authHeaders(url);
+        const url = this.constructUrl("media/" + mid, parameters);
+        const headers =  this.authHeaders(url);
         headers['Accept'] = 'application/json';
         return axios.get(url, {headers});
     }
 
-
-   public async iterate( parameters:Record<string, string> = {}, body = ""): Promise<any> {
-
-        let query = this.joinParameters(parameters);
-        let url = this.base_url + "media/iterate" +  query;
-        let headers =  this.authHeaders(url);
+    public async iterate(parameters:Record<string, string> = {}, body = ""): Promise<any> {
+        const url = this.constructUrl("media/iterate", parameters);
+        const headers =  this.authHeaders(url);
         headers['Content-Type'] = 'application/xml';
         return axios.post(url, body, {
             headers,
             responseType: 'stream'
-        }
+            }
         );
     }
 
-   private joinParameters(parameters:Record<string, string> = {}): string {
-     let result= Object.entries(parameters)
-            .map(([key, value]) => `${key}=${value}`)
-            .join('&');
-      return result === "" ? "" : "?" + result;
-   }
 
-
-
-   private authHeaders(url: string): Record<string, string> {
-        const u = new URL(url);
+    protected authHeaders(url: URL): Record<string, string> {
         const npoDate =  new Date().toUTCString();
-        const queryMsg =  this.sortAndConcat(u.searchParams);
-        const msg = `origin:${this.origin},x-npo-date:${npoDate},uri:${u.pathname}${queryMsg}`;
+        const queryMsg =  this.sortAndConcat(url.searchParams);
+        const msg = `origin:${this.origin},x-npo-date:${npoDate},uri:${url.pathname}${queryMsg}`;
         const enc = CryptoJS.HmacSHA256(msg, this.secret).toString(CryptoJS.enc.Base64);
         return {
             'Origin': this.origin,
@@ -63,12 +50,28 @@ class NpoApi {
             'Authorization': `NPO ${this.key}:${enc}`
         };
     }
+
+    // utilities
+
+    protected constructUrl(path: string, parameters:Record<string, string> = {}) : URL {
+        let query = this.joinParameters(parameters);
+        let url = this.base_url + path +  query;
+        return new URL(url);
+    }
+
+    private joinParameters(parameters:Record<string, string> = {}): string {
+        let result= Object.entries(parameters)
+            .map(([key, value]) => `${key}=${value}`)
+            .join('&');
+        return result === "" ? "" : "?" + result;
+    }
     private sortAndConcat(query: URLSearchParams): string {
         return Array.from(query.entries())
             .sort((a, b) => a[0].localeCompare(b[0]))
             .map(([key, value]) => `,${key}:${value}`)
             .join('');
     }
+
 
 }
 export default NpoApi;
